d0579474087b7d7cdc547b0bf1c0d6f08b50778e
Erfan <mrenmityerf@gmail.com>
2024-10-25 18:31:10 +0330

    points is done

--- src/components/Points.jsx
+++ src/components/Points.jsx
@@ -1,26 +1,67 @@
 import React from "react";
+import { arrowlong, pointsContent, container2, container3 } from "../constants";
 
 const Points = () => {
   return (
-    <section>
-      <div className=" mx-auto grid grid-cols-2 ">
-        <div className="text-right flex flex-col justify-end items-end">
-          <p className="text-lg text-justify font-mulish">
-            We transform innovative ideas into <br></br> captivating and
-            memorable digital <br></br> experiences that engage and inspire.
+    <section className="w-full px-[70px] my-[120px]">
+      <div className=" text-customBlue flex items-end justify-between">
+        <div className="text-right flex flex-col font-mulish">
+          <p className="text-md text-justify font-mulish mb-5">
+            We connect your brand with the right <br /> influencers, forging
+            authentic partner <br /> that amplify your message.
           </p>
+          <div className="flex justify-between items-center font-bold">
+            <p className="text-lg">See all services</p>
+            <img src={arrowlong} alt="" />
+          </div>
         </div>
 
         <div>
           <p className="text-[#ff593e] font-barlow italic font-bold text-2xl uppercase tracking-wide">
-            *Let us show you!
+            *YOU KNOW WHAT?!
           </p>
-          <h1 className="text-6xl font-black leading-none font-barlow">
-            HOW <span className="text-[#ffc4c4]">WE DRIVE</span> YOUR <br />{" "}
-            BRAND
-            <span className="text-[#ffc4c4]"> TO NEW HEIGHT</span>
+          <h1 className="text-7xl font-black leading-none font-barlow ">
+            WE DESIGN NEANINGFUL NOT <br /> JUST WICK IMPRESSIONS
           </h1>
         </div>
+      </div>
+      <div className="grid grid-rows-2 grid-cols-2 w-full h-full gap-5 my-[70px]">
+        {pointsContent.map((item, i) => (
+          <div
+            key={i}
+            style={{
+              backgroundImage: `url(${
+                i === 0 || i === 3 ? container3 : container2
+              })`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+            className="p-[50px] w-full h-[24.1rem] "
+          >
+            <div className="w-full flex items-center justify-between mt-[20px] mb-[40px]">
+              <h2
+                className={`text-5xl font-bold font-mulish uppercase ${
+                  i === 0 || i === 3 ? "text-white" : "text-customBlue"
+                }`}
+              >
+                {item.title.split(" ").map((word, idx) => (
+                  <React.Fragment key={idx}>
+                    {word}
+                    {idx < item.title.split(" ").length - 1 && <br />}
+                  </React.Fragment>
+                ))}
+              </h2>
+              <img src={item.icn} width={75} alt="" />
+            </div>
+            <p
+              className={`${
+                i === 0 || i === 3 ? "text-white" : "text-customBlue"
+              } text-lg font-semibold`}
+            >
+              {item.text}
+            </p>
+          </div>
+        ))}
       </div>
     </section>
   );
--- src/constants/index.js
+++ src/constants/index.js
@@ -12,6 +12,10 @@ export { default as image11 } from "../assets/11.jpg";
 export { default as arrow } from "../assets/arrow.png";
 export { default as camera } from "../assets/Polaroid Camera.png";
 
+import user from "../assets/user.svg";
+import announce from "../assets/announce.svg";
+import add from "../assets/add.svg";
+import target from "../assets/target.svg";
 export { default as arrowuprightwhite } from "../assets/arrowuprightwhite.svg";
 export { default as arrowupright } from "../assets/arrowupright.svg";
 export { default as logo } from "../assets/logo.svg";
@@ -24,6 +28,8 @@ import cadburys from "../assets/cadburys.svg";
 import dunkin from "../assets/dunkin.svg";
 import huawei from "../assets/huawei.svg";
 export { default as container1 } from "../assets/container1.svg";
+export { default as container2 } from "../assets/container2.svg";
+export { default as container3 } from "../assets/container3.svg";
 export { default as like } from "../assets/like.svg";
 export { default as plus } from "../assets/plus.svg";
 export { default as quote } from "../assets/quote.svg";
@@ -31,6 +37,7 @@ export { default as sticker } from "../assets/sticker.svg";
 export { default as target } from "../assets/target.svg";
 export { default as target2 } from "../assets/target2.svg";
 export { default as colorLogo } from "../assets/colorLogo.svg";
+export { default as arrowlong } from "../assets/arrowlong.svg";
 
 export const headerButtons = [
   { title: "Home", id: 1 },
@@ -63,5 +70,28 @@ export const BenefitsContent = [
   {
     title: "Insight Analysis",
     text: "Receive comprehensive, detailed reports and insights that allow you to track progress and measure success effectively, our analytics provide a clear overview.",
+  },
+];
+
+export const pointsContent = [
+  {
+    icn:target,
+    title: "Social Strategy",
+    text: "Our Social Strategy is all about developing customized plans that align seamlessly with your brand’s goals and the behaviors of your target audience. We dive deep into the unique aspects.",
+  },
+  {
+    icn:user,
+    title: "Creator Management",
+    text: "Our Creator Management services are designed to empower influencers to thrive in their creative endeavors, offering them the personalized support and strategic guidance they need.",
+  },
+  {
+    icn:add,
+    title: "Influencer Partnerships",
+    text: "We specialize in connecting your brand with the right influencers, forging authentic partnerships that amplify your message and expand your reach.",
+  },
+  {
+    icn:announce,
+    title: "Social Publishing",
+    text: "Our Social Publishing services streamline your content delivery, ensuring your brand’s voice is consistently heard across all platforms. We take the hassle out of managing.",
   },
 ];
