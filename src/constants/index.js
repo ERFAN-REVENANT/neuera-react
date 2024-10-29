f488db582b3a1ca8a97a49303d42e997784f17df
Erfan <mrenmityerf@gmail.com>
2024-10-29 16:37:32 +0330

    1

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,8 +3,7 @@ import image2 from "../assets/2.png";
 import image3 from "../assets/3.png";
 import image6 from "../assets/6.png";
 import image7 from "../assets/7.png";
-import image8 from "../assets/8.png";
-import image9 from "../assets/9.png";
+
 import image10 from "../assets/10.png";
 export { default as image4 } from "../assets/4.jpg";
 export { default as image5 } from "../assets/5.png";
